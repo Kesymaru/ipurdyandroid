50fa1a456c810138646cf34009b682acfbda1449
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2013-02-08 17:49:01 -0600

    desarrollo

--- js/main.js
+++ js/main.js
@@ -1,72 +1,198 @@
 /**
 * SE ENCARGA DE TODO LO RELACIONADO CON EL ESTILO
 */
+//var resultados = $.mobile.path.parseUrl("http://77digital.digimoblabs.com/webServiceJson.php?username=ipurdy&password=12345678&action=vehiculos");
+
+var tipos = [];
+var estados = [];
+var asesores = [];
+var sucursales = [];
+var table = '';
+
+var params = 'js/testdata.json';
+//var params = 'http://77digital.digimoblabs.com/webServiceJson.php?username=ipurdy&password=12345678&&action=vehiculos';
+
+$(document).bind('mobileinit',function(){
+	//$.mobile.selectmenu.prototype.options.nativeMenu = false;
+	$.mobile.fixedToolbars.hide(true);
+});
 
 $(document).ready(function(){
-	var alto = $('html').height() - ( $('.bottonset').innerHeight() + $(".selectors").innerHeight() );
 	
-	//alert(alto);
+	$('.cancelar-dialogo').click(function(){
+		$('.ui-dialog').dialog('close');
+	});
 
-	$("#lista-consultas tbody").css({height:alto});
-
-
+	$("#resultados tr").each(function(){
+		$(this).click(function(){
+			InfoPage($(this).attr('id'));
+		});
+	});
+	
+	$("#form-sincronizar").live('submit',function(event){
+		event.preventDefault();
+    	event.stopPropagation();
+		alert('no envia');
+		return false;
+	})
 });
-	
+
 /**
-* MUESTRA / OCULTA CONTENTs
+* CARGA LOS DATOS GUARDADOS
 */
-function colum2(){
-	//si esta visible oculta
-	if( $("#colum2").is(":visible") ){
-		$("#colum1").css({"display":"inline-block"});
-
-		$("#colum1").animate({
-			width: "100%",
-		}, { 
-			duration: 1000, 
-			queue: true,
-			complete: function(){
-			}
-		});
-
-		$("#colum2").animate({
-			width: "0%",
-		}, { 
-			duration: 1000, 
-			queue: true,
-			complete: function(){
-				$("#colum2").hide()
-			}
-		});
+function Data(){
+	if(true){
 
 	}else{
-		$("#colum2").css({"display":"inline-block"});
-
-		$("#colum1").animate({
-			width: "0%",
-		}, { 
-			duration: 1000, 
-			queue: true,
-			complete: function(){
-				$("#colum1").hide();
-			}
-		});
-
-		$("#colum2").animate({
-			width: "100%",
-		}, { 
-			duration: 1000, 
-			queue: true,
-			complete: function(){
-			}
-		});
 
 	}
 }
 
 /**
-* CARGA LAS CONSULTAS
+* SINCRONIZA
 */
-function Consultas(){
-	
+function Sincronizar(){
+	$.ajax({
+		url: params,
+	    type: 'get',
+	    async: true,
+		dataType: 'json',
+		cache: true,
+		beforeSend: function(){
+			$.mobile.showPageLoadingMsg();
+		},
+		success: function(data){
+			localStorage.resultados = data;
+
+			if( !jQuery.isEmptyObject(localStorage.resultados) > 0 ){
+				var date = new Date();
+				
+				var horas = date.getHours();
+				var minutos = date.getMinutes();
+				var ampm = horas >= 12 ? 'pm' : 'am';
+				horas = horas % 12;
+				horas = horas ? horas : 12; 
+				minutos = minutos < 10 ? '0'+minutos : minutos;
+
+				lastUpdate = date.getMonth()+1+"/"+date.getDate()+"/"+date.getFullYear()+" - "+horas+":"+minutos+ampm;
+				$("#home-footer h3")
+				.hide()
+				.html("Ultima actualizacion: "+lastUpdate)
+				.fadeIn();
+
+			}else{
+				$("#home-footer h3").html("Ultima actualizacion: cookie");
+			}
+
+			//carga la datbla sin filtrar
+			$.each(data.INFOUNIDAD, function(f, c){
+				//compone la fila
+				var tr = '<tr id="'+c.UNIDAD+'">'+
+							'<td><b class="ui-table-cell-label">Unidad</b>'+
+								c.UNIDAD+'</td>'+
+							'<td><b class="ui-table-cell-label">Color</b>'+c.DESC_COLOR_EXT+'</td>'+
+							'<td><b class="ui-table-cell-label">Modelo</b>'+
+								c.MODELO+'</td>'+
+							'<td><b class="ui-table-cell-label">Fecha llegada</b>';
+				
+				if( jQuery.isEmptyObject(c.FECHA_LLEGADA) ){
+					$.each(c.FECHA_LLEGADA, function(x,valor){
+						tr += '---';
+					})
+				}else{
+					tr += c.FECHA_LLEGADA;
+				}
+						tr	+='</td>'+
+							'<td><b class="ui-table-cell-label">Cliente</b>';
+				if( jQuery.isEmptyObject(c.NOMBRE_CLIENTE) ){
+					tr += '---';
+				}else{
+					tr += c.NOMBRE_CLIENTE;
+				}
+						tr	+='</td>';
+
+						tr	+='<td><b class="ui-table-cell-label">Sucursal</b>';
+				//sucursal
+				if( jQuery.isEmptyObject(c.SUCURSAL) ){
+					tr += '---';
+				}else{
+					tr += c.SUCURSAL;
+				}
+						tr	+='</td>'+
+						  '</tr>';
+				
+				table += tr;
+
+				tipos.push(c.TIPO_VEHICULO);
+			});
+			
+			//elimina elementos duplicados 
+			EliminarDuplicados();
+
+			//compone los selects
+			Selects();
+
+			$("#resultados tbody").append(table)
+			//$("#resultados").table( "refresh" );
+		},
+		fail: function(){
+			Error();
+		}
+	}).done(function(){
+		$.mobile.hidePageLoadingMsg();
+
+		$("#resultados tr").each(function(){
+			$(this).click(function(){
+				InfoPage($(this).attr('id'));
+			});
+		});
+	});
+}
+
+function EliminarDuplicados(){
+	//elimina elementos duplicacos
+	tipos = tipos.filter(function(elem, pos) {
+		return tipos.indexOf(elem) == pos;
+	});
+	localStorage.tipos = tipos;
+
+	/*
+	estados = estados.filter(function(elem, pos) {
+		return estados.indexOf(elem) == pos;
+	});
+
+	asesores = asesores.filter(function(elem, pos) {
+		return asesores.indexOf(elem) == pos;
+	});
+
+	sucursales = sucursales.filter(function(elem, pos) {
+		return sucursales.indexOf(elem) == pos;
+	});
+	*/
+}
+
+/**
+* CARGA LOS FILTROS
+*/
+function Selects(){
+
+}
+
+/**
+* CARGA INFORMACION EN INFO PAGE
+* id -> id de la info
+*/
+function InfoPage(id){
+	//var datos = resultados.id;
+	alert(id);
+	$.mobile.changePage('#info', { transition: "slide"} );
+}
+
+/************************* HELPES *******************/
+
+/**
+* 
+*/
+function Error(){
+	$.mobile.changePage('#error', {role:'dialog'});
 }
